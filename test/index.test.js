605e5b0f998af8f46680dafb0056150b62ef83fb
Miroslav Popov <info@forexsb.com>
2023-07-22 10:42:31 +0300

    Better test output.

--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,44 @@
-const {readFileSync} = require("fs");
-const {equal}        = require("assert");
-const {describe, it} = require("node:test");
+"use strict";
+
+const {readFileSync}        = require("fs");
+const {equal}               = require("assert");
+const {describe, it, after} = require("node:test");
 
 const { readJson } = require("../index");
 
 const encodings = ["ansi", "ut8", "utf8-bom", "utf16-be-bom", "utf16-le-bom"];
 
 describe("Read JSON file", () => {
+    const output = [];
+
     for (const encoding of encodings) {
         it(encoding, () => {
             const filename = `./test/foo.${encoding}.json`;
-            const json     = readJson(filename);
-            
+
+            /** @type {{foo: number}} */
+            const json = readJson(filename);
+
             equal(json.foo, 42);
-            
-            // log
-            const fileContent = readFileSync(filename);
-            console.log(`ok => Encoding: ${encoding.padEnd(12, " ")}, data: ${fileContent}`);
+
+            // Log details
+            output.push({
+                encoding,
+                data: readFileSync(filename)
+                    .map((byte) => byte === 0 ? 32 : byte > 127 || byte < 32 ? 63 : byte)
+                    .toString(),
+                text: JSON.stringify(json),
+            });
         })
     }
+
+    after(() => {
+        // Print output
+        const encMaxLen  = output.reduce((res, item) => item.encoding.length > res ? item.encoding.length : res, 0);
+        const dataMaxLen = output.reduce((res, item) => item.data.length > res ? item.data.length : res, 0);
+        for (const rec of output) {
+            const enc  = rec.encoding.padEnd(encMaxLen);
+            const data = rec.data.padEnd(dataMaxLen);
+            console.log(`Encoding: ${enc}, data: ${data}, json: ${rec.text}`);
+        }
+    })
 })
